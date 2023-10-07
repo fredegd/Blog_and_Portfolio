824ea528b29efa765a588028651c69de713095d6
fredegd <124848643+fredegd@users.noreply.github.com>
2023-10-07 22:32:00 +0200

    blogItem heading

--- src/components/BlogItem.jsx
+++ src/components/BlogItem.jsx
@@ -22,6 +22,7 @@ import { KeyboardArrowUp, KeyboardArrowLeft } from "@mui/icons-material";
 import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 import RelatedPosts from "./RelatedPosts";
 import Footer from "./Footer";
+import BlogItemHeading from "./BlogItemHeading";
 export default function BlogItem() {
   const theme = useTheme();
   const [blog, setBlog] = useState();
@@ -216,121 +217,7 @@ export default function BlogItem() {
             alignItems: "center",
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-
-              alignItems: "flex-start",
-              backgroundColor: theme.palette.background.main,
-              maxWidth: "1280px",
-
-              boxShadow: `0px 0px 10px 0px ${theme.palette.text.highlightAlt}`,
-            }}
-          >
-            <Box
-              sx={{
-                height: "60vw",
-                width: "90vw",
-                maxWidth: "1280px",
-                backgroundImage: `url(${blog.blogTitleImage.fields.file.url})`,
-                backgroundPosition: "center",
-                backgroundSize: `100% auto`,
-                // backgroundRepeat: "no-repeat",
-                transition: "all 0.5s ease-in-out",
-              }}
-            >
-              {/* content image */}
-            </Box>
-
-            <Typography
-              variant="h1"
-              sx={{
-                fontSize: {
-                  xs: "6.2vw",
-                  sm: "6.5vw",
-                  md: "5.5vw",
-                  lg: "4.5vw",
-                },
-                padding: "1rem",
-                transition: "all 0.5s ease-in-out",
-                fontWeight: "bold",
-                textAlign: "justify",
-              }}
-            >
-              {blog.title}
-            </Typography>
-
-            <Typography
-              variant="h4"
-              sx={{
-                fontSize: { xs: "5vw", sm: "4vw", md: "3.5vw", lg: "2.55vw" },
-                fontStyle: "italic",
-                marginY: "1.5rem",
-                padding: { xs: "1rem", md: 0 },
-                pb: "1rem",
-              }}
-            >
-              {""}"{blog.subtitle}"
-            </Typography>
-            <Box
-              sx={{
-                maxWidth: "900px",
-
-                display: "flex",
-                flexDirection: "column",
-                px: { xs: 2, md: 0 },
-                fontSize: { xs: "2.5vw", sm: "1rem", md: "1.2rem", lg: "1rem" },
-                gap: "0.3rem",
-              }}
-            >
-              <Typography variant="p">
-                Published on: "{blog.createdAt}" by "
-                {blog.blogAuthor.fields.authorName}"
-              </Typography>
-              <Typography variant="p">
-                Last edit: "{blog.editAt}" {} min. Read
-              </Typography>
-
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "flex-start",
-                }}
-              >
-                <Typography variant="p" sx={{ textAlign: "center" }}>
-                  Tags:
-                </Typography>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    flexWrap: "wrap",
-                    gap: "1rem",
-                  }}
-                >
-                  {blog.tags.tags.map((tag, index) => {
-                    return (
-                      <Typography
-                        key={index}
-                        variant="p"
-                        sx={{
-                          mx: "1rem",
-                          backgroundColor: theme.palette.text.highlightAlt,
-                          color: theme.palette.primary.contrastText,
-                          padding: "0.2rem 0.8rem",
-                          borderRadius: "1rem",
-                        }}
-                      >
-                        {tag}
-                      </Typography>
-                    );
-                  })}
-                </Box>
-              </Box>
-            </Box>
-          </Box>
+<BlogItemHeading blog={blog} />
 
           <Box
             sx={{
