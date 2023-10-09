b87c95a191c0d535db0c9fc43a9a4440b21a7586
fredegd <124848643+fredegd@users.noreply.github.com>
2023-10-09 21:07:39 +0200

    style fix

--- src/components/FullScreenPreview.jsx
+++ src/components/FullScreenPreview.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-
-
 import CloseIcon from "@mui/icons-material/Close";
 
-import {Box,Typography, Slide,IconButton,Toolbar, AppBar, Dialog} from "@mui/material"
+import {
+  Box,
+  Typography,
+  Slide,
+  IconButton,
+  Toolbar,
+  AppBar,
+  Dialog,
+} from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -17,9 +22,8 @@ export default function FullScreenPreview({
   setOpenPreview,
   image,
   title,
-  description
+  description,
 }) {
-
   const theme = useTheme();
 
   const handleClose = () => {
@@ -34,25 +38,49 @@ export default function FullScreenPreview({
         onClose={handleClose}
         TransitionComponent={Transition}
       >
-        <AppBar sx={{ position: "relative", background:theme.palette.background.main}}>
+        <AppBar
+          sx={{
+            position: "relative",
+            background: theme.palette.background.main,
+          }}
+        >
           <Toolbar>
             <IconButton
               edge="start"
               color="inherit"
               onClick={handleClose}
               aria-label="close"
+              sx={{ color: theme.palette.text.primary }}
             >
               <CloseIcon />
             </IconButton>
-            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="Box">
+            <Typography
+              sx={{ ml: 2, flex: 1, color: theme.palette.text.primary }}
+              variant="h6"
+              component="Box"
+            >
               {title}
             </Typography>
           </Toolbar>
         </AppBar>
-        <Box sx={{display:"flex",flexDirection:"column",justifyContent:"center", alignItems:"center" , p:{xs:"0.5rem", md: "2rem"}}} onClick={handleClose}>
-
-        <img src={image} alt={title} width={"90%"}/>
-        <Typography variant="p"sx={{ width:{xs:"100%",md:"90%"} ,textAlign:"justify"}}  > {description}</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            p: { xs: "0.5rem", md: "2rem" },
+          }}
+          onClick={handleClose}
+        >
+          <img src={image} alt={title} width={"90%"} />
+          <Typography
+            variant="p"
+            sx={{ width: { xs: "90%", md: "90%" }, textAlign: "justify" }}
+          >
+            {" "}
+            {description}
+          </Typography>
         </Box>
       </Dialog>
     </Box>
