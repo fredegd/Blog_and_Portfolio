07fa61de94b1937d4a8719bd5d4b611d4d686422
fredegd <124848643+fredegd@users.noreply.github.com>
2023-10-12 16:53:08 +0200

    react-snap root

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
+import { hydrate, render } from "react-dom";
+
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import DarkModeProvider from "./context/DarkModeContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  // </React.StrictMode>,
+
+const APP = (
   <BrowserRouter>
       <DarkModeProvider>
         <App />
       </DarkModeProvider>
   </BrowserRouter>
 );
+
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  hydrate(APP, rootElement);
+} else {
+  render(APP, rootElement);
+}
