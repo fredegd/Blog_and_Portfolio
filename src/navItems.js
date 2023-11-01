604d6e6b95905e7ed7b5713c7d3c76cb8ee38fee
fredegd <124848643+fredegd@users.noreply.github.com>
2023-11-02 00:59:59 +0100

    works route instead of Projects

--- src/navItems.js
+++ src/navItems.js
@@ -2,6 +2,6 @@ export const navItems = [
   { id: "home", name: "Home", linkTo: "/" },
   { id: "blog", name: "Blog", linkTo: "/blog" },
   { id: "about", name: "About", linkTo: "/about" },
-  { id: "projects", name: "Projects", linkTo: "/projects" },
+  { id: "works", name: "Works", linkTo: "/works" },
   { id: "contact", name: "Contact", linkTo: "/contact" },
 ];
