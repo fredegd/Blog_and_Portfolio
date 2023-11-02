aae3386bc838917917f67dc17de6aa9421b3e343
fredegd <124848643+fredegd@users.noreply.github.com>
2023-11-02 12:33:33 +0100

    blogItemHeading style fix

--- src/components/BlogItemHeading.jsx
+++ src/components/BlogItemHeading.jsx
@@ -4,14 +4,12 @@ import { useTheme } from "@mui/material/styles";
 import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-
 export default function BlogItemHeading({ blog }) {
   const theme = useTheme();
   const createdAt = new Date(blog.sys.createdAt).toLocaleDateString();
   // +" at:" +new Date(blog.sys.createdAt).toLocaleTimeString();
   const updatedAt = new Date(blog.sys.updatedAt).toLocaleDateString();
   // + " at:" + new Date(blog.sys.updatedAt).toLocaleTimeString();
-
 
   const options = {
     renderNode: {
@@ -66,7 +64,7 @@ export default function BlogItemHeading({ blog }) {
     });
     return Math.ceil(contentArray.reduce((a, b) => a + b, 0) / 5 / 250); //250 is the average word count per minute and 5 is the average length of a word
   };
-   console.log(blogLength());
+  console.log(blogLength());
 
   return (
     <Box
@@ -74,7 +72,7 @@ export default function BlogItemHeading({ blog }) {
         display: "flex",
         flexDirection: "column",
 
-        alignItems: "flex-start",
+        alignItems: "center",
         background: {
           xs: `linear-gradient(90deg, #00000000 0%,${theme.palette.background.transparent} 5%, ${theme.palette.background.main} 20%, ${theme.palette.background.main} 80%, ${theme.palette.background.transparent} 95%,  #00000000 100%)`,
           lg: `linear-gradient(90deg, #00000000 0%,${theme.palette.background.transparent} 15%, ${theme.palette.background.main} 30%, ${theme.palette.background.main} 70%, ${theme.palette.background.transparent} 85%,  #00000000 100%)`,
@@ -134,21 +132,26 @@ export default function BlogItemHeading({ blog }) {
       <Box
         sx={{
           width: "100%",
+          maxWidth: "900px",
+          paddingX: { xs: "1rem", md: "0rem" },
           display: "flex",
           flexDirection: { xs: "column", lg: "row" },
-          justifyContent: { lg: "space-between" },
+          justifyContent: { xs: "flex-start", lg: "space-between" },
         }}
       >
         <Box
           sx={{
-            padding: { xs: "1rem", md: 1.5 },
+            paddingY: { xs: "1rem", md: 1.5 },
 
             display: "flex",
             alignItems: "center",
             justifyContent: "flex-start",
           }}
         >
-          <Typography variant="p" sx={{ textAlign: "left" }}>
+          <Typography
+            variant="p"
+            sx={{ textAlign: "left", fontWeight: "bold" }}
+          >
             Tags:
           </Typography>
           <Box
@@ -186,18 +189,20 @@ export default function BlogItemHeading({ blog }) {
             display: "flex",
             flexDirection: "column",
             alignItems: "flex-start",
-            px: { xs: 2, md: 0 },
-            fontSize: { xs: "2.5vw", sm: "1rem", md: "1.2rem", lg: "1rem" },
+            // px: { xs: 2, md: 0 },
+            textAlign: "justify",
+            fontSize: { xs: "2.8vw", sm: "1rem", md: "1.2rem", lg: "1rem" },
             gap: "0.5rem",
-            padding: { xs: "1rem", md: "2rem" },
+            paddingY: { xs: "1rem", md: "2rem" },
           }}
         >
           <Typography variant="p">
-            Published on: "{createdAt}" by "
-            {blog.fields.blogAuthor.fields.authorName}"
+            <strong>Published on: </strong>
+            {createdAt} by "{blog.fields.blogAuthor.fields.authorName}"
           </Typography>
           <Typography variant="p">
-            Last edit: "{updatedAt}" {blogLength()} min. Read
+            <strong>Last edit: </strong>
+            {updatedAt} {blogLength()} min read
           </Typography>
         </Box>
       </Box>
