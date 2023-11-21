1b4a98d92e70df1a3937ce3ec65b0fc00bbc6d3f
fredegd <124848643+fredegd@users.noreply.github.com>
2023-11-21 20:24:01 +0100

    nav items order

--- src/navItems.js
+++ src/navItems.js
@@ -2,6 +2,6 @@ export const navItems = [
   { id: "home", name: "Home", linkTo: "/" },
   { id: "works", name: "Works", linkTo: "/works" },
   { id: "blog", name: "Blog", linkTo: "/blog" },
-  { id: "contact", name: "Contact", linkTo: "/contact" },
   { id: "about", name: "About", linkTo: "/about" },
+  { id: "contact", name: "Contact", linkTo: "/contact" },
 ];
